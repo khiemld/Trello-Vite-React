416ca7a727a1cde9659164006a87747d01315b90
Duy Khiêm Lê <khiemit240@gmail.com>
2023-07-28 22:52:04 +0700

    chore: build base layout

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,17 @@
 import { experimental_extendTheme as extendTheme} from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { deepOrange, red, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
-        primary: {
-          main: '#E65F2B',
-          contrastText: '#fff'
-        },
-        secondary: {
-          main: '#F2EAE5'
-        }
+        primary: teal,
+        secondary: deepOrange
       }
     },
     dark: {
